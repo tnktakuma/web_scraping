63ff76f7270278c4de192573c8691ec4faae99fb
tnktakuma <tak.1025.rmk@gmail.com>
2020-12-26 00:52:11 +0900

    fix bug

--- global_ip.py
+++ global_ip.py
@@ -1,17 +1,24 @@
 import os
-import requests
+import sys
 
 from slack_api import Slack, MY_CID
 
 
-global_ip = None
-filename = os.path.join(os.environ['HOME'], '.global_ip')
-if os.path.exists(filename):
-    with open(filename, 'r') as f:
-        global_ip = f.read()
-response = requests.get('http://inet-ip.info/ip')
-ip_address = response.text
-if ip_address != global_ip:
-    Slack().post_message(MY_CID, ip_address)
-    with open(filename, 'w') as f:
-        f.write(ip_address)
+def main(ip_address: str):
+    global_ip = None
+    filename = os.path.join(os.environ['HOME'], '.global_ip')
+    if os.path.exists(filename):
+        with open(filename, 'r') as f:
+            global_ip = f.read()
+    if ip_address != global_ip:
+        Slack().post_message(MY_CID, ip_address)
+        with open(filename, 'w') as f:
+            f.write(ip_address)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        raise ValueError(
+            'Usage: python ' + sys.argv[0] + ' $(curl ifconfig.io -4)')
